7a8dc3bf9b6fcfba365963daa15da0219d3c552c
bvvivek6 <vivekbv242813@gmail.com>
2025-06-16 10:24:52 +0530

    Add database connection setup with mongoose in server configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const connectDB = require("./config/db");
 const cors = require("cors");
 app.use(
   cors({
@@ -11,6 +12,8 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on PORT: ${port}`);
